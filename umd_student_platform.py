a5aaa3e25826220b990f44ea267eec40cb35cecd
madewole1 <81781629+madewole1@users.noreply.github.com>
2021-04-16 00:52:25 -0400

    Added benchmark_1 class, two methods, and a function.
    Free-styled a bit with the P/F, but we can discuss that later.

--- umd_student_platform.py
+++ umd_student_platform.py
@@ -1,2 +1,33 @@
 """A platform for UMD students to look up prerequistes for classes
 """
+
+class Benchmark_1:
+  """Determines whether a student has met their Benchmark 1 requirement
+  Attributes: 
+    course (str): course id 
+    grade (float): grade student receives 
+  """
+  def __init__(self, course, grade):
+    """Assigning attributes to objects 
+    Args: 
+       course (str): course id 
+       grade (float): grade student receives 
+    """
+    self.course = course
+    self.grade = grade 
+    
+  def pass_fail (course, grade):
+    """This function will determine whether the student has Pass/Failed the class 
+    and adds the course to their audit/profile
+    
+    Args: 
+      See __init__ method
+    Returns: passed courses 
+    """
+ def credit_counter (course):
+  """Counts credits student has passed and how many more they need
+  
+  Args: 
+    course (str): course id 
+  Returns: number of outstanding credits 
+  """
