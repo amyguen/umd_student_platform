849adf885ec5beaeb3c3e50558f886e8811a5829
Amelia Guenterberg <a.guenterberg@gmail.com>
2021-05-03 19:04:41 -0400

    formatted methods and functions

--- umd_student_platform.py
+++ umd_student_platform.py
@@ -1,7 +1,6 @@
 
 """ A platform for UMD I-School students to look up class prerequisites, plan 
-    their schedules, find contact information for advisors, and view potential
-    career options.
+    their schedules, and find contact information for advisors.
 """
 
 class Student:
@@ -20,18 +19,21 @@ class Student:
             student_id (int): the unique identifying number of the student
             student_name (str): the name of the student
             grad_year (int): the intended graduation year of the student
+            classes_taken (list of str) : the list of classes the student has
+              taken 
         
         Side effects:
-            creates global variables, specifically student_id, student_name, and
-                grad_year
+            creates global variables, specifically student_id, student_name, 
+              grad_year, and classes_taken
         """
         self.student_name = student_name
         self.student_id = student_id
         self.grad_year = grad_year
         self.classes_taken = classes_taken
 
+
     def greet(self):
-      """This function takes a student's name and and student's identification 
+      """ This method takes a student's name and and student's identification 
       number to create a customized greeting based on the inputs of the student 
       trying to use our program.
 
@@ -39,11 +41,9 @@ class Student:
         self (Student) : an instance of the Student Class
 
       Returns: 
-        welcome (string):This function returns our welcome statements with the 
-          student's name and identification number in the greeting. 
-
+        welcome (str) : This method returns our welcome statements with the 
+          student's name and identification number in the greeting.
       """
-   
       welcome = input(f'Welcome, {self.student_name} (UID: {self.student_id}),'+
                     ' to I-School help! What can we assist you with? ' +
                     'Please choose from the following list of options: \n' +
@@ -53,19 +53,15 @@ class Student:
                     ' Year')
       return welcome
    
+
     def get_classes(self):
-      """
-      This function uses a loop to allow students to enter the classes that 
+      """ This method uses a loop to allow students to enter the classes that 
       they have taken that are part of the Information Science major, stores 
       them in a list, and then returns the list with the classes in it. 
 
-      Args:
-        This function has no set arguments. 
-
       Returns: 
-        student_classes(list): the inputted classes that the student has 
+        student_classes (list) : the inputted classes that the student has 
         taken in the Information Science major. 
-
       """
       count = 0
       self.classes_taken = []
@@ -79,7 +75,10 @@ class Student:
           count += 1     
       return self.classes_taken
 
+
     def benchmark_I():
+      """
+      """
       requirements = input('Have you taken MATH115 or higher and psych100? ')
       if requirements == 'no':
           incomplete = 'You have not completed the benchmark I courses.'
@@ -91,7 +90,10 @@ class Student:
           response = 'Please try again and enter either yes or no.'
           return response
       
+
     def benchmark_II():
+      """
+      """
       requirements = input('Have you taken STAT100, INST126, and INST201? ')
       if requirements == 'no':
           incomplete = 'You have not completed the benchmark II courses.'
@@ -102,12 +104,39 @@ class Student:
       else:
           response = 'Please try again and enter either yes or no.'
           return response
-          
+    
+    
+    def core_courses(taken):
+      """Determines a list of core INST courses that the student must still
+      take
+        
+      Args:
+        taken (list): list of strings of INST classes that the student has 
+            already received credit for
+        
+      Returns:
+        a list of core INST courses
+      """
+
+
+    def specializations(specialization):
+      """Finds all the classes in the specified specialization parameter
+      
+      Args:
+        specialization (string): a string of the specialization (cybersecurity,
+            data science, healthcare, digital curation)
+        
+      Returns:
+        a list of courses in the specified information science specialization 
+      """
+
+
     def credit_counter(classes_taken):
+      """
+      """
       credits = len(classes_taken) * 3
       return credits
       
-  
 
     def advising_contacts(student_id, student_name, specialization):
         """ Returns to the student a list of advisors and their contact 
@@ -125,56 +154,28 @@ class Student:
                 recommended for the student
         """
 
+
     def data_retrieval(url_input):
-        """
-        This function will use imported modules to web scrape the UMD's
+        """ This function will use imported modules to web scrape the UMD's
         School of Information's website so that we can retrieve data on request.
 
         Args: 
-            (string)url: the address to the site where we will derive 
+            url_input (str) : the address to the site where we will derive 
                 information from
         
         Returns: the part of the website that is searched for 
         """
     
-    def specializations(specialization):
-      """Finds all the classes in the specified specialization parameter
-      
-      Args:
-        specialization (string): a string of the specialization (cybersecurity,
-            data science, healthcare, digital curation)
-        
-      Returns:
-        a list of courses in the specified information science specialization 
-      """
-      
-    def core_courses(taken):
-      """Determines a list of core INST courses that the student must still
-      take
-        
-      Args:
-        taken (list): list of strings of INST classes that the student has 
-            already received credit for
-        
-      Returns:
-        a list of core INST courses
-      """
-        
-    def pass_fail(course, grade):
-      """This function will determine whether the student has Pass/Failed the class 
-        and adds the course to their audit/profile
-        
-        Args:
-            course (str) : the course the student inputs
-            grade (str) : the grade received in the course
-        Returns: 
-            passed courses 
-      """
 
-      def write_to_file(student):
-        pass
+    def write_to_file(student):
+      """
+      """
+      pass
+
 
 def load_from_file(student_id):
+  """
+  """
   with open('Students/'+ student_id, 'r', encoding = 'utf-8') as f:
     for line in f:
       attributes = line.strip().split(',')
@@ -184,7 +185,10 @@ def load_from_file(student_id):
       class_instance = Student(student_id, student_name, grad_year, taken_classes)
       return class_instance
   
+
 def main():
+  """
+  """
   while True:
     been_here = input('Hi! Have you been here before? \n')
     if been_here.lower() == 'no':
@@ -203,29 +207,23 @@ def main():
       print('Please enter yes or no')
   
   option = class_instance.greet()
-  
   if option == "Benchmark I":
-      print(class_instance.benchmark_I())
-      
+      print(class_instance.benchmark_I())  
   elif option == 'Benchmark II':
       print(class_instance.benchmark_II())
-  
   elif option == "Core Courses":
       class_instance.get_classes()
-      print("C")
-      
+      print("C") 
   elif option == "Specializations":
       class_instance.get_classes()
-      print("S")
-      
+      print("S") 
   elif option == "Advising Contacts":
-      print("A")
-      
+      print("A") 
   elif option == "Credit Counter":
       print('You have completed '+ str(credit_counter(get_classes())) + ' INST credits.')
-
   else:
       print("I'm sorry, we don't know how to help you with that.")
+
 
 if __name__ == "__main__":
   main()
